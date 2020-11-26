c35ea23b95f747d4ecc1df96161545d886bf2f19
dvranga <devangmranganath@gmail.com>
2020-11-26 20:48:24 +0530

    [ranganath]Refactor:The code is refactore to write a function to get work hours

--- empWage.js
+++ empWage.js
@@ -1,35 +1,21 @@
-//UC 1 IF ELSE Condition
-{
-    const IS_ABSENT=0;
-    let empCheck=Math.floor(Math.random()*10)%2;
-    if(empCheck==IS_ABSENT){
-        console.log("UC1-Employee is ABSENT");
-    }
-    else{
-        console.log("UC1 -Employee is PRESENT");
-    }
-}
-
-//UC 2 Switch Case
 const IS_PART_TIME=1;
 const IS_FULL_TIME=2;
 const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGEE_PER_HOUR=20;
 
-let empHrs=0;
-let empCheck=Math.floor(Math.random()*10)%3;
-switch (empCheck){
-    case IS_PART_TIME:
-        empHrs=PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        empHrs=FULL_TIME_HOURS;
-        break;
-    default:
-        empHrs=0;    
-    
+function getWorkingHours(empCheck){
+    switch (empCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;       
+    }
 }
 
+let empCheck=Math.floor(Math.random()*10)%3;
+let empHrs=getWorkingHours(empCheck);
 let empWage=empHrs*WAGEE_PER_HOUR;
-console.log("UC2-Emp Wage: "+empWage);
+console.log("Hours "+empHrs+" Emp Wage: "+empWage);
