efd109953a5a4cadf9ec90edbb2d6aba5cb735d1
dvranga <devangmranganath@gmail.com>
2020-11-29 00:22:35 +0530

    [ranganath]Added:Daily employee wage array to store daily wage

--- empWage.js
+++ empWage.js
@@ -2,9 +2,10 @@ const IS_PART_TIME=1;
 const IS_FULL_TIME=2;
 const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
-const WAGEE_PER_HOUR=20;
+const WAGE_PER_HOUR=20;
 const NUM_OF_WORKING_DAYS=20;
 const MAX_HRS_IN_MONTH=160;
+let empArray=new Array();
 
 function getWorkingHours(empCheck){
     switch (empCheck){
@@ -17,10 +18,10 @@ function getWorkingHours(empCheck){
     }
 }
 
-// for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
-//     let empCheck=Math.floor(Math.random()*10)%3;
-//     totalEmpHrs +=getWorkingHours(empCheck);    
-// }
+function getEmpDailyWage(totalEmpHrs){
+    return totalEmpHrs*WAGE_PER_HOUR;
+}
+
 
 let totalEmpHrs=0;
 let totalWorkingDays=0;
@@ -28,9 +29,20 @@ while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
     totalWorkingDays<NUM_OF_WORKING_DAYS) {
     totalWorkingDays++;
     let empCheck=Math.floor(Math.random()*10)%3;
-    totalEmpHrs += getWorkingHours(empCheck);
+    let dailyWrkHrs= getWorkingHours(empCheck);
+    totalEmpHrs+=dailyWrkHrs;
+    empArray.push(getEmpDailyWage(dailyWrkHrs));
 }
 
-let empWage=totalEmpHrs*WAGEE_PER_HOUR;
-console.log("Total Days: "+totalWorkingDays
-        +"  Total Hrs: "+totalEmpHrs+" Emp Wage: "+empWage);
+let empWage=0;
+for(let dailyWage of empArray) empWage+=dailyWage;
+
+empWage=getEmpDailyWage(totalEmpHrs);
+console.log("Total Days: ",totalWorkingDays
+        ,"  Total Hrs: ",totalEmpHrs," Emp Wage: ",empWage);
+
+
+// for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+//     let empCheck=Math.floor(Math.random()*10)%3;
+//     totalEmpHrs +=getWorkingHours(empCheck);    
+// }
