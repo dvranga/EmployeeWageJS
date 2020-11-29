a622f474b50e6fede5e10f9dd8c129c26733da86
dvranga <devangmranganath@gmail.com>
2020-11-30 00:35:31 +0530

    [ranganath]Added:Stored daily wage in map in java script

--- empWage.js
+++ empWage.js
@@ -6,6 +6,7 @@ const WAGE_PER_HOUR=20;
 const NUM_OF_WORKING_DAYS=20;
 const MAX_HRS_IN_MONTH=160;
 let empDailyWageArr=new Array();
+let empDailyWageMap=new Map();
 
 function getWorkingHours(empCheck){
     switch (empCheck){
@@ -127,3 +128,32 @@ const findWorkingDays=(workingDays,day)=>{
 }
 let totalDays=empDailyWageArr.reduce(findWorkingDays,0);
 console.log('totalWorkingDays',totalDays);
+
+//UC_8
+
+ totalEmpHrs=0;
+ totalWorkingDays=0;
+ let empDailyWageArr1=new Array();
+ let empDailyWageMap1=new Map();
+
+const calcDailyWage=(empHrs)=>{
+    return empHrs*WAGE_PER_HOUR;
+}
+
+while(totalEmpHrs<=MAX_HRS_IN_MONTH &&
+    totalWorkingDays < NUM_OF_WORKING_DAYS){
+        totalWorkingDays++;
+        let empCheck=Math.floor(Math.random()*10)%3;
+        let empHrs=getWorkingHours(empCheck);
+        totalEmpHrs += empHrs;
+        empDailyWageArr1.push(calcDailyWage(empHrs))
+        empDailyWageMap1.push(totalWorkingDays,calcDailyWage(empHrs));
+    }
+
+    console.log(empDailyWageMap1);
+    function totalWages(totalWage, dailyWage){
+        return totalWage+dailyWage;
+    }
+
+    console.log("UC_7A - Emp Wage Mapp total Hrs : ",
+    Array.from(empDailyWageMap1.values()).reduce(totalWages,0));
