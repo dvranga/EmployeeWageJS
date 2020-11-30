837340ad4d585122ae9074854673bb2aacae7e3c
dvranga <devangmranganath@gmail.com>
2020-11-30 23:47:02 +0530

    [ranganath]Added:Ability to check the name start with capital and atleast 3 characters using Regex Pattern with try catch

--- NewEmployeePayrollData.js
+++ NewEmployeePayrollData.js
@@ -1,10 +1,12 @@
 class EmployeePayrollData{
     //property
+    id;
     salary;
     gender;
     startDate;
+    
 
-
+//constructor
     constructor(...params){
         this.id=params[0];
         this.name=params[1];
@@ -17,15 +19,21 @@ get name() {
      return this._name;
     }
 set name(name){
-    console.log("setting "+name)
-    this._name=name;
+    let nameRegex=RegExp('^[A-Z]{1}[a-z]{3,}$');
+    if(nameRegex.test(name)) {
+        console.log('setting',name)
+         this._name=name;
+        }
+    else throw 'Name is Incorrect';
 }
 
 //method
 toString(){
     const options={year: 'numeric ',month: 'long',day:'numeric'};
-    const empDate=this.startDate===undefined?"unidefined":this.startDate.toLocalDateString("en-US",options);
-      return "id = "+this.id+" , name= "+this.name+" , salary = "+this.salary+
+    const empDate=!this.startDate?"unidefined":
+    this.startDate.toLocalDateString("en-US",options);
+      return "id = "+this.id+" , name= "+this._name+" , salary = "
+      +this.salary+
       " , "+"gender = "+this.gender +" , startDate= "+empDate;
 }
 
@@ -34,8 +42,13 @@ toString(){
 let employeePayrollData =new EmployeePayrollData(1,"Mark",30000);
 console.log(employeePayrollData.toString());
 employeePayrollData.id=0;
-employeePayrollData.name="Jeff";
-console.log(employeePayrollData.toString());
+
+try{
+    employeePayrollData.name="john";
+    console.log(employeePayrollData.toString());
+}catch(e){
+    console.error(e);
+}
 
 let newEmployeePayrollData=new EmployeePayrollData(1,"Terrisa",30000,"F",new Date());
 console.log(newEmployeePayrollData);
